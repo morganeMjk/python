a7d108667a0a80c2455f399b8909a1f34869a899
morganeMk <morgane.mjk@outlook.fr>
2022-11-29 11:46:20 +0100

    Ajout d'un cours sur les variables
    Création du fichier de cours.
    Ajout de variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+# l'opérateur d'affectation  = permet d'injecter une valeur dans une variable
+
+my_number1 = 123
+my_number2 = -42
+
+my_text1 = "ceci est une chaine de caractère"
+my_text2 = "ceci est aussi une chaine de caractère"
+
+print(my_text1)
+print(my_text2)
+print(my_number1)
+print(my_number2)
