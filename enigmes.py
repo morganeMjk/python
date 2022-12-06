c869ce037a03781b607b2f66a7cf4228f89aa331
morganeMk <morgane.mjk@outlook.fr>
2022-12-06 11:31:15 +0100

    Ajout du fichier operators

---
+++ enigmes.py
@@ -0,0 +1,4 @@
+foo = 2.71
+a = int(foo)
+b = foo - a
+print(a, b)
---
+++ operators.py
@@ -0,0 +1,44 @@
+# importer une librairie Random et Math
+import random
+import math
+
+#  = affectation
+foo = 123
+
+# + addition
+foo = 123 + 42
+
+# += incrémentation
+foo += 42
+
+# - soustraction
+foo = 123 - 42
+
+# -= décrémentation
+foo -= 42
+
+# / division
+foo = 123 / 42
+
+# // division entière
+foo = 123 // 42
+
+# % modulo
+foo = 4 % 3
+foo = random.randint(1,100)
+print(foo % 2)
+
+# * multiplication
+foo = 123 * 42
+
+# ** puissance
+# ^ puissance mais pas en python
+foo = 2 ** 2
+
+# math.sqrt() racine carré
+foo = math.sqrt(4)
+foo = 4 ** 0.5
+foo = 4 ** (1/2)
+
+# ** (1/3) racine cubique
+foo = 8 ** (1/3)
